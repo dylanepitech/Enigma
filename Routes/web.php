de98c3725ffc6ca8703a4ab71fdd74b7d551b2e7
NightDev83 <dylan.bouraoui83@gmail.com>
2024-04-10 11:40:59 +0200

    Switch Router auto, router statique by .env

--- Routes/web.php
+++ Routes/web.php
@@ -1,9 +1,8 @@
 <?php 
 namespace Routes;
 use Routes\index;
-require_once __DIR__ . '/../vendor/autoload.php';
+// require_once __DIR__ . '/../vendor/autoload.php';
 $invok = new index();
 $invok->Route('/','HomeController@view', 'GET');
-$invok->Route('/home','IndexController@view', 'GET');
 
 index::SELECT_ROUTE($invok);
--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <?php
 namespace index;
-use Routes\web;
+require_once __DIR__ . '/vendor/autoload.php';
+use Dotenv\Dotenv;
+try {
+    $dotenv = Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+    
+    $test = $_ENV['ROUTER_AUTO'];
+    var_dump($test);
 
-require_once('Routes/web.php');
+} catch (\Throwable $e) {
+    echo "Error: " . $e->getMessage();
+}
+
+if ($test == 'false')
+{
+    require_once __DIR__ . '/Routes/web.php';
+
+}else {
+    var_dump('Coucou');
+}
